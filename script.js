d1926b0627d7fe3ad426905e3f5108425021ec9c
Gavin Popken <gavinpopkenart@gmail.com>
2023-12-04 19:04:59 -0600

    added the correct classes to the fungus based on the if we win or lose, and disable the attacks if HP is below 0

--- script.js
+++ script.js
@@ -6,17 +6,17 @@
 // Todo 
 // - STATE:create variable for fungus' health points and out attack points (can't go negative - check to make sure you have enough AP to make the attack?)
 //// - RENDER: update how much AP and HP show on the screen
-// - if the mushroom dies, replace it's 'walk' class with 'dead' class
+//// - if the mushroom dies, replace it's 'walk' class with 'dead' class
 // - if you don't have AP, make the mushroom's class be 'jump', and disable attribute to the attack buttons
 // - STRETCH
 
 function onReady() {
     console.log("Ready to go!")
-    
+
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
+
+
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
@@ -30,12 +30,39 @@ onReady()
 // ! Event
 
 // When on of the attacks is clicked
-function attack(cost, damage){
-console.log('Attack calld. \nCost:', cost, '\nDamage:', damage);
-//updates state 
-enemyHP -= damage
-ourAP -= cost
-render()
+function attack(cost, damage) {
+    console.log('Attack calld. \nCost:', cost, '\nDamage:', damage);
+    //updates state 
+    enemyHP -= damage
+    ourAP -= cost
+    checkIfDead();
+    render()
+}
+
+function checkIfDead() {
+    //console.log(document.getElementsByClassName('freaky-fungus'));
+    if (enemyHP <= 0) {
+        document.getElementsByClassName('freaky-fungus')[0].classList.replace('walk', 'dead')
+        //resets hp so that it doesn't go below 0
+        enemyHP = 0
+    }
+    if (ourAP <= 0) { // In the case of a tie, it will appear as though the fungus loses
+        document.getElementsByClassName('freaky-fungus')[0].classList.replace('walk', 'jump')
+        ourAP = 0
+        // get the buttons to disable them
+        disableAttacks();
+    }
+
+    // A function to disable the attack buttons
+function disableAttacks() {
+    const buttons = document.getElementsByTagName('button')
+    //console.log(buttons);
+    for (let button of buttons) {
+        button.setAttribute('disabled', 'true')
+    }
+}
+
+
 }
 
 
@@ -54,7 +81,7 @@ function render() {
     `
 
     const hPTextDiv = document.getElementsByClassName('hp-text')[0]
-    console.log(hPTextDiv);
+    //console.log(hPTextDiv);
     hPTextDiv.innerHTML = `
     ${enemyHP} HP
     `
